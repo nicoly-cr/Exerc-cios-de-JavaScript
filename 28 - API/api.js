d35df5810118b6ff75a80bff5cf4a5e04b61ed07
NICOLY CORRÊA ROVIERO <nicoly.roviero@portalsesisp.org.br>
2026-05-05 12:06:07 -0300

    Exercícios JS

--- 28 - API/api.js
+++ 28 - API/api.js
@@ -73,3 +73,114 @@ async function carregarPosts2() {
     }
 }
 carregarPosts2();
+
+
+//---------------------------------------------------------------------------------
+
+
+//async function buscarDados(){
+//    try{
+//        const resposta = await fetch("https://api.exemplo.com/dados");
+//        const dados = await resposta.json();
+//        console.log(dados);
+//    }catch(erro){
+//        console.log("Erro ao buscar API");
+//    }
+//}
+//buscarDados();
+
+
+//---------------------------------------------------------------------------------
+
+
+async function buscarUsuarios(){
+    const resposta = await fetch("https://jsonplaceholder.typicode.com/users");
+    const dados = await resposta.json();
+    console.log(dados);
+}
+
+
+//---------------------------------------------------------------------------------
+
+
+async function enviarDados() {
+    const novoUsuario = {
+        name: "João Silva",
+        email: "joao.silva@example.com"
+    };
+
+    const resposta = await fetch("https://jsonplaceholder.typicode.com/users", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(novoUsuario)
+    });
+
+    const usuarioCriado = await resposta.json();
+    console.log(usuarioCriado);
+}
+enviarDados();
+
+
+//---------------------------------------------------------------------------------
+
+
+async function atualizarUsuario(){
+    const usuarioAtualizado = {
+        id: 1,
+        name: "João Silva",
+        email: "joao.silva@example.com"
+    };
+
+    const resposta = await fetch("https://jsonplaceholder.typicode.com/users/1", {
+        method: "PUT",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(usuarioAtualizado)
+    });
+
+    const usuarioAtualizadoResult = await resposta.json();
+    console.log(usuarioAtualizadoResult);
+}
+atualizarUsuario();
+
+
+//---------------------------------------------------------------------------------
+
+
+async function atualizarParcialUsuario(){
+    const usuarioParcialAtualizado = {
+        name: "João Silva",
+    };
+
+    const resposta = await fetch("https://jsonplaceholder.typicode.com/users/1", {
+        method: "PATCH",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(usuarioParcialAtualizado)
+    });
+
+    const usuarioParcialAtualizadoResult = await resposta.json();
+    console.log(usuarioParcialAtualizadoResult);
+}
+atualizarParcialUsuario();
+
+
+//---------------------------------------------------------------------------------
+
+
+async function excluirUsuario() {
+    const resposta = await fetch("https://jsonplaceholder.typicode.com/users/1", {
+        method: "DELETE"
+    });
+
+    if(resposta.ok){
+        console.log("Usuário excluído com sucesso");
+    }else{
+        console.log("Erro ao excluir usuário");
+    }
+}
+excluirUsuario();
